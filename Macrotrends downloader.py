7e8bbb2179e41757cd217b2ab7a304c3b06a6d5e
AnnaBezu <91892320+AnnaBezu@users.noreply.github.com>
2022-01-26 17:14:12 +0100

    Create Macrotrends downloader.py

---
+++ Macrotrends downloader.py
@@ -0,0 +1,24 @@
+import requests
+import re
+import json
+from bs4 import BeautifulSoup as bs
+
+
+url = 'https://www.macrotrends.net/stocks/charts/MMM/3m/financial-ratios?freq=Q' #quarterly data
+response = requests.get(url)
+p = re.compile(r' var originalData = (.*?);\r\n\r\n\r',re.DOTALL)
+data = json.loads(p.findall(response.text)[0])
+headers = list(data[0].keys())
+headers.remove('popup_icon')
+ratios = []
+for row in data:
+    soup = bs(row['field_name'])
+    field_name = soup.select_one('a, span').text
+    fields = list(row.values())[2:]
+    fields.insert(0, field_name)
+    ratios.append(fields)
+
+pd.option_context('display.max_rows', None, 'display.max_columns', None)
+df = pd.DataFrame(ratios, columns = headers)
+print(df.head())
+df
