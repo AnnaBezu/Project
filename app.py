fa17f4f84b76b7795fd0620a0fd7c08ac3aaedcd
AnnaBezu <91892320+AnnaBezu@users.noreply.github.com>
2022-02-03 00:32:56 +0100

    Update app.py
    - Subsets of data working (working for plots)
    - line chart added for Close price

--- app.py
+++ app.py
@@ -20,9 +20,11 @@ data are uploading and it may take a while... This process may take approximatel
 
 st.markdown("<h6 style='text-align: cleft; color: #6aa84f; '> Selection of data for analysis </h6>", unsafe_allow_html=True)
 
-       
 tickers=SP500()
-x=get_data_try() #CHANGE THIS FUNCTION TO get_data_yahoo() TO GET ALL DATA!
+data=get_data_try() #CHANGE THIS FUNCTION TO get_data_yahoo() TO GET ALL DATA!
+with st.spinner('Data loading'):
+    time.sleep(5)
+st.success('Data are loaded!')
 
 #data=get_data() #NOT USE - TAKES A LONG LONG LONG TIME
 
@@ -55,13 +57,41 @@ date_final = col2_date_final.date_input('Select the final day of analysis')
 #tickerData = yf.Ticker('MSFT')
 #tickerDf = tickerData.history(period='1d', start='2010-1-1', end='2022-1-25')
 
+data_volume=pd.DataFrame(data.Volume[selected_tickers],columns=selected_tickers)
+data_close=pd.DataFrame(data.Close[selected_tickers],columns=selected_tickers)
+data_open=pd.DataFrame(data.Open[selected_tickers],columns=selected_tickers)
+#data['Date'] = pd.to_datetime(data['Date'],format='%Y%m%d')
+#data['Date']=data['Date'].dt.date
+#data['Date']=data['Date'].dt.date
+#data.index=data.index.to_pydatetime()
+#data_date=data.Date
+
+
+
  
+type=data.columns
 st.markdown("<h6 style='text-align: cleft; color: #6aa84f; '> Please, press the button to see if the analysis will continue to work correctly with the selected data.  </h6>", unsafe_allow_html=True)
 
-if st.button('Some button'):
-    if len(selected_tickers) >4:
-        st.write('You can select maximum 4 tickers for analysis. Please, reselect your tickers otherwise, the analysis may not be correct and some error may occur.')
+if st.button('Click for check'):
+    if len(selected_tickers) > 4:
+        st.warning('You can select maximum 4 tickers for analysis. Please, reselect your tickers otherwise, the analysis may not be correct and some error may occur.')
+    elif len(selected_tickers) <1:
+        st.error('You must select at least 1 ticker.')
     else:
         st.write('With selected data, the analysis will work properly :-)')
+        st.write("Your selected tickers are:")
+        st.write(', '.join(selected_tickers))
+        
+        st.markdown('** Close price**.')
+        col_close, col_close_t = st.columns([3, 2])
 
+        col_close.subheader("Close price of the stocks")
+        col_close.line_chart(data_close)
+        with st.expander("See explanation"):
+                 st.write("""
+         The chart and the table above show the data for close price for selected stocks. 
+     """)
 
+        col_close_t.subheader("A narrow column with the data")
+        col_close_t.write(data_close)
+        
