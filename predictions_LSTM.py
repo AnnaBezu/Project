5ca403c45ec19989d19f73866aaa90546bab0877
JurackovaMartina <79155184+JurackovaMartina@users.noreply.github.com>
2022-02-06 15:46:42 +0100

    Add files via upload

---
+++ predictions_LSTM.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import numpy as np
+import yfinance as yf
+import keras 
+
+from keras.preprocessing.sequence import TimeseriesGenerator
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt # for graph
+
+import SP500_data_downloader as SP
+from SP500_data_downloader import *
+
+
+
+# define data generator that will train your model
+
+class DataGeneratorSeq(object):
+
+    def __init__(self,prices,batch_size,num_unroll):
+        self._prices = prices
+        self._prices_length = len(self._prices) - num_unroll
+        self._batch_size = batch_size
+        self._num_unroll = num_unroll
+        self._segments = self._prices_length //self._batch_size
+        self._cursor = [offset * self._segments for offset in range(self._batch_size)]
+
+    def next_batch(self):
+
+        batch_data = np.zeros((self._batch_size),dtype=np.float32)
+        batch_labels = np.zeros((self._batch_size),dtype=np.float32)
+
+        for b in range(self._batch_size):
+            if self._cursor[b]+1>=self._prices_length:
+                #self._cursor[b] = b * self._segments
+                self._cursor[b] = np.random.randint(0,(b+1)*self._segments)
+
+            batch_data[b] = self._prices[self._cursor[b]]
+            batch_labels[b]= self._prices[self._cursor[b]+np.random.randint(0,5)]
+
+            self._cursor[b] = (self._cursor[b]+1)%self._prices_length
+
+        return batch_data,batch_labels
+
+    def unroll_batches(self):
+
+        unroll_data,unroll_labels = [],[]
+        init_data, init_label = None,None
+        for ui in range(self._num_unroll):
+
+            data, labels = self.next_batch()    
+
+            unroll_data.append(data)
+            unroll_labels.append(labels)
+
+        return unroll_data, unroll_labels
+
+    def reset_indices(self):
+        for b in range(self._batch_size):
+            self._cursor[b] = np.random.randint(0,min((b+1)*self._segments,self._prices_length-1))
+
+
+
+            
+# define function for final predictions
+
+def predict(num_prediction, model):
+    prediction_list = close_data_orig[-look_back:]
+    
+    for _ in range(num_prediction):
+        x = prediction_list[-look_back:]
+        x = x.reshape((1, look_back, 1))
+        out = model.predict(x)[0][0]
+        prediction_list = np.append(prediction_list, out)
+    prediction_list = prediction_list[look_back-1:]
+        
+    return prediction_list
